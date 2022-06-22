89f8d07203a8f99b4622706d8e15e09dca2f458e
Galina Demidova <galinagriz77@gmail.com>
2022-06-22 12:05:53 +0300

    commit code correct

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -3,16 +3,13 @@ package ru.netology.manager;
 
 public class PosterManager {
 
-
     protected Poster[] posters = new Poster[0];
-    private int maxLimit = 9;
-    private int minLimit;
-    private int resultLength;
+    private int maxLimit = 10;
 
 
-    public PosterManager(int resultLength) {
-        //Poster[] posters = new Poster[limit];
-        this.maxLimit = minLimit + resultLength;
+    public PosterManager(int limit) {
+
+        this.maxLimit = limit;
 
     }
 
@@ -39,29 +36,14 @@ public class PosterManager {
         return posters;
     }
 
-    public int getMaxLimit() {
-        return maxLimit;
-    }
-
-    public void setMaxLimit(int maxLimit) {
-        this.maxLimit = maxLimit;
-    }
-
-    public int getResultLength() {
-        return resultLength;
-    }
-
-    public void setResultLength(int resultLength) {
-        this.resultLength = resultLength;
-    }
 
     public Poster[] findLast() {
+        int resultLength;
 
-
-        if (resultLength < posters.length) {
+        if (maxLimit > posters.length) {
             resultLength = posters.length;
         } else {
-            resultLength = resultLength;
+            resultLength = maxLimit;
 
         }
 
